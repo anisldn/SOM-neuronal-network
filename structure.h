c9f0e227bb8b0a7e001e186843cd09a1d0b6f5ed
anisldn <anis.laidani34@gmail.com>
2019-02-16 05:51:00 +0100

    init

---
+++ structure.h
@@ -0,0 +1,17 @@
+
+typedef struct {
+	double *vect;
+	char *nom;
+	double norme;
+} data_iris;
+
+typedef struct {
+	double *vect;
+	char *nom;
+	//double norme;
+} neurone;
+
+typedef struct {
+	neurone **neuron;
+} grille;
+
